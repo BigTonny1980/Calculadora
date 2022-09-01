7d0a64dcc61b1b7b7b9bfb1d6370909ccf2ce506
Antonio <tonny858895>
2022-08-31 23:07:51 -0300

    alterações 1.2

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-function inserir(num) {
+function inserirNumero(num) {
    let numero = document.getElementById("displayCalculadora").textContent;
    document.getElementById("displayCalculadora").textContent = numero + num;
+}
+
+let operacao; // VARIÁVEL GLOBAL
+let primeiraParcela; // VARIÁVEL GLOBAL
+
+function inserirOperacao(parametro) {
+   operacao = parametro;
+   primeiraParcela = document.getElementById("displayCalculadora").textContent;
+   primeiraParcela = parseInt(primeiraParcela);
+   limpar(); 
 }
 
 function limpar() {
    document.getElementById("displayCalculadora").textContent = "";
 }
 
-function calcular(calc) {
-   // E AGORA?
+let variavelNova; // VARIÁVEL GLOBAL
+
+function calcular() {
+   variavelNova = document.getElementById("displayCalculadora").textContent;
+   variavelNova = parseInt(variavelNova);
+
+   if (operacao === "+") {
+      variavelNova = primeiraParcela + variavelNova;
+   }
+
+   if (operacao === "-") {
+      variavelNova = primeiraParcela - variavelNova;
+   }
+
+   if (operacao === "*") {
+      variavelNova = primeiraParcela * variavelNova;
+   }
+
+   if (operacao === "/") {
+      variavelNova = primeiraParcela / variavelNova;
+   }
+
+   document.getElementById("displayCalculadora").textContent = variavelNova;
 } 
